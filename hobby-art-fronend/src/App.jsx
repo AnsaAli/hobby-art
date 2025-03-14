b8af8edacfa99ce9c6622f11d3025c39df5f8415
AnsaAli <chinjuansa@example.com>
2025-03-14 11:08:04 +0530

    add components

--- hobby-art-fronend/src/App.jsx
+++ hobby-art-fronend/src/App.jsx
@@ -1,9 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import Body from "./components/Body.jsx";
 
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Body/>}/>
+    </Routes>
+    </BrowserRouter>
+  );
 }
